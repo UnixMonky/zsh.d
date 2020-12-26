6db97efcff552bfa6ccb751cb15e80a5083235a3
Matt Huber <unixmonky@gmail.com>
2020-12-26 08:52:51 -0500

    update aliases

---
+++ nnn.zsh
@@ -0,0 +1,11 @@
+# nnn
+export NNN_BMS='h:~;d:~/Downloads;s:~/Sync'
+export NNN_PLUG='p:preview-tui'
+# Use the chart below. Take the number, and find the decimal version with the converter
+# https://www.rapidtables.com/converter/number/decimal-to-hex.html
+# https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
+export NNN_FCOLORS='0000E6310000000000000000'
+alias nnn "nnn -e"
+# alias ls "nnn -e"
+export NNN_FIFO="/tmp/nnn.fifo"
+export TERMINAL=st
---
+++ terraform.zsh
@@ -0,0 +1,4 @@
+## terraform
+alias tf='terraform'
+alias tfp='terraform plan --out m1.plan'
+alias tfa='terraform apply m1.plan'
